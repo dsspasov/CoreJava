2b54b247eceb93989b4b696a42a67e52c9f0775b
dsspasov <dsspasov94@gmail.com>
2015-04-06 15:45:53 +0300

    solution

---
+++ week2/Collection1/src/todotask/ToDoList.java
@@ -0,0 +1,62 @@
+package todotask;
+
+import java.util.PriorityQueue;
+
+public class ToDoList {
+
+    PriorityQueue<Task> queue = new PriorityQueue<Task>();
+    private float hours;
+
+    ToDoList(float hours) {
+        this.setHours(hours);
+    }
+
+    void addTask(Task t) {
+        queue.offer(t);
+    }
+
+    void markFinished(Task t) {
+        if (!t.getFinished()) {
+            t.setFinished(true);
+        }
+    }
+
+    void markCancelled(Task t) {
+        t.setFinished(false);
+        this.queue.remove(t);
+
+    }
+
+    Task getTop() {
+        return this.queue.peek();
+        //return this.queue.element();
+    }
+    
+    String top(){
+        return getTop().getClass().getName().substring(9);
+    }
+
+    boolean canFinish() {
+        return getRemainigTime() < getTimeNeeded();
+    }
+
+    float getRemainigTime() {
+        return (hours - getTimeNeeded());
+    }
+
+    int getTimeNeeded() {
+        int timeNeeded = 0;
+        for (Task t : queue) {
+            timeNeeded += t.getTime();
+        }
+        return timeNeeded;
+    }
+
+    public float getHours() {
+        return hours;
+    }
+
+    public void setHours(float hours) {
+        this.hours = hours;
+    }
+}
